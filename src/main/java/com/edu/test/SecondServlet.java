b61af8d1dab916a11abe30f4cff1c72934765f53
JJeonBH <wwjjss5386@gmail.com>
2024-02-04 23:25:54 +0900

    요청방식에 따른 실행

---
+++ src/main/java/com/edu/test/SecondServlet.java
@@ -0,0 +1,24 @@
+package com.edu.test;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/second")	//	서블릿 실행을 위한 URI를 "/second"로 설정
+public class SecondServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("SecondServlet!!");
+	}
+	/*
+	클라이언트가 /second로 요청하면 SecondServlet의 init() 메소드가 실행되어야 하는데, 
+	재정의하지 않아서 GenericServlet 클래스의 init() 메소드가 실행된다. 또한, init() 실행 후 
+	service() 메소드가 실행되어야 하는데, 역시 재정의하지 않아서 HttpServlet 클래스의 
+	service(ServletRequest, ServletResponse) 메소드가 실행되고 이 메소드는 
+	service(HttpServletRequest, HttpServletResponse) 메소드를 호출한다.
+	이 메소드는 doGet() 메소드를 호출한다.(Get 방식의 요청을 보냄)
+	*/ 
+}
