3d1e88cc3d9fe249b3f60915cf72eed240b58a4c
JJeonBH <wwjjss5386@gmail.com>
2024-02-15 18:47:54 +0900

    doGet, doPost Overriding

---
+++ src/main/java/com/edu/test/QueryTestServlet.java
@@ -0,0 +1,35 @@
+package com.edu.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/queryTest")
+public class QueryTestServlet extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<html><head><title>Query 문자열 테스트</title></head>");
+		out.print("<body>");
+		out.print("<h1>GET 방식으로 요청되었습니다</h1>");
+		out.print("</body></html>");
+		out.close();
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<html><head><title>질의 문자열 테스트</title></head>");
+		out.print("<body>");
+		out.print("<h1>POST 방식으로 요청되었습니다</h1>");
+		out.print("</body></html>");
+		out.close();
+	}
+}
