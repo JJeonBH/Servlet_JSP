aca05745263cfb7f04408cd6e44fd2182ec50c05
JJeonBH <wwjjss5386@gmail.com>
2024-02-26 22:51:22 +0900

    HttpServlet을 통해 ServletContext 객체 추출하기

--- src/main/java/com/edu/test/ServletContextTest1Servlet.java
+++ src/main/java/com/edu/test/ServletContextTest1Servlet.java
@@ -7,16 +7,10 @@ import jakarta.servlet.http.*;
 
 @WebServlet("/context1")
 public class ServletContextTest1Servlet extends HttpServlet {
-	ServletContext sc;
-
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		sc = config.getServletContext();
-	}
-
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = resp.getWriter();
+		ServletContext sc = this.getServletContext();
 		out.print("Context : " + sc);
 		out.close();
 	}
