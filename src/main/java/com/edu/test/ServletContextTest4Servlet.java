4aaa90f68963c623050977442dc4e87acd6e4b56
JJeonBH <wwjjss5386@gmail.com>
2024-02-29 21:44:32 +0900

    웹 애플리케이션 단위 정보 공유

---
+++ src/main/java/com/edu/test/ServletContextTest4Servlet.java
@@ -0,0 +1,24 @@
+package com.edu.test;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+@WebServlet("/context4")
+public class ServletContextTest4Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+
+		ServletContext sc = this.getServletContext();
+
+		ShareObject obj1 = (ShareObject) sc.getAttribute("data1");
+		ShareObject obj2 = (ShareObject) sc.getAttribute("data2");
+
+		out.print("DATA 1 : " + obj1.getCount() + " , " + obj1.getStr() + "<br>");
+		out.print("DATA 2 : " + obj2.getCount() + " , " + obj2.getStr() + "<br>");
+
+		out.close();
+	}
+}
